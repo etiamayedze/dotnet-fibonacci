9112da2e7ab5caa7a383065b53b9b2d5d7cb035a
Straw-Etiam <etiam.ayedze@gmail.com>
2021-10-11 15:44:35 +0200

    add database

---
+++ src/Fibonacci/Migrations/20211011133025_AddFibFibonacciCreatedTimestamp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fibonacci.Migrations
+{
+    public partial class AddFibFibonacciCreatedTimestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

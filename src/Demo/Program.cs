f026617a9d5a13b44fbb7f7be342ae91857785f3
Straw-Etiam <etiam.ayedze@gmail.com>
2021-10-18 14:53:50 +0200

    add logger and service collection

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -3,29 +3,43 @@ using System.Diagnostics;
 using System.IO;
 using Fibonacci;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
 IConfiguration configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddEnvironmentVariables()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)  
-    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{environmentName}.json", true, true)
     .Build();
+var applicationSection = configuration.GetSection("Application");
+var applicationConfig = applicationSection.Get<ApplicationConfig>();
 
-var applicationName = configuration.GetValue<string>("Application:Name");
-var applicationMessage = configuration.GetValue<string>("Application:Message");
 
-Console.WriteLine($"Application Name : {applicationName}");
-Console.WriteLine($"Application Message : {applicationMessage}");
+var services = new ServiceCollection();  
+services.AddTransient<FibonacciDataContext>();  
+services.AddTransient<Compute>();  
+services.AddLogging(configure => configure.AddConsole());
 
-Stopwatch stopwatch = new();
-stopwatch.Start();
+using (var serviceProvider = services.BuildServiceProvider())
+{
+    var logger =serviceProvider.GetService<ILogger<Compute>>();
+    logger.LogError($"Application Name : {applicationConfig.Name}");
+    logger.LogInformation($"Application Message : {applicationConfig.Message}");
 
-using var fibonacciDataContext = new FibonacciDataContext();
 
-var tasks = await new Fibonacci.Compute(fibonacciDataContext).ExecuteAsync(args);
+    Stopwatch stopwatch = new();
+    stopwatch.Start();
+    var compute = serviceProvider.GetService<Compute>();
+    var tasks = await compute.ExecuteAsync(args);
+    foreach (var task in tasks) Console.WriteLine($"Fibo result : {task}");
+    stopwatch.Stop();
+    Console.WriteLine($"{stopwatch.Elapsed.Seconds}s");
+}
 
-foreach (var task in tasks) Console.WriteLine($"Fibo result: {task}");
-stopwatch.Stop();
-Console.WriteLine($"{stopwatch.Elapsed.Seconds} s");
+public class ApplicationConfig
+{
+    public string Name { get; set; }
+    public string Message { get; set; }
+}
